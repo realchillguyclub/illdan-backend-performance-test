21fecdb3c94357ecde844f469d3cae25e4b48bad
신재웅 (JaeWoong Shin) <tlswodnd4316@gmail.com>
2025-12-10 16:57:28 +0900

    [feat]: 타겟 RPS를 변경한다 (#9)

--- scripts/todo/backlogs.load.js
+++ scripts/todo/backlogs.load.js
@@ -31,16 +31,19 @@ export const options = {
     backlogs_load: {
       executor: 'ramping-arrival-rate',
       exec: 'backlogsScenario',
-      startRate: 10,          // 시작 RPS
+      startRate: 5,          // 시작 RPS
       timeUnit: '1s',
       preAllocatedVUs: 100,
-      maxVUs: 500,
+      maxVUs: 1000,
       stages: [
-        { target: 20, duration: '3m' },
-        { target: 30, duration: '3m' },
-        { target: 40, duration: '3m' },
-        { target: 50, duration: '3m' },
-        { target: 60, duration: '3m' },
+        { target: 10, duration: '2m' },
+        { target: 20, duration: '2m' },
+        { target: 30, duration: '2m' },
+        { target: 40, duration: '2m' },
+        { target: 50, duration: '2m' },
+        { target: 60, duration: '2m' },
+        { target: 70, duration: '2m' },
+        { target: 80, duration: '2m' },
         // 램프다운
         { target: 0,  duration: '2m' },
       ],
